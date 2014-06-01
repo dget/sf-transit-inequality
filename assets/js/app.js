da36e73566b68632612de223fcb9dc1f03e86af7
Lauren Coleman <laurencoleman@USSFMLT12947.local>
2014-06-01 15:38:56 -0700

    Add key, text color changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,6 +23,7 @@ angular.module('app').controller('AppCtrl', ['$scope', 'DATA_SOURCES', function(
         });
     });
 
+
     // Load and show the map of CA
     d3.json("data/ca-topo.json", function (error, ca_topojson){
         map_data = ca_topojson;
@@ -66,8 +67,8 @@ angular.module('app').controller('AppCtrl', ['$scope', 'DATA_SOURCES', function(
         });
 
         // dimensions
-        var w = 580,
-            h = 260,
+        var w = 780,
+            h = 360,
             hMargin = 65,
             vMargin = 20,
             dotRadius = 3,
